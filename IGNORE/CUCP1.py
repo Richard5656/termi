0835ac9f057b5ad2f0cc75ee33c91b015dd871a0
AudioTore <95601200+AudioTore@users.noreply.github.com>
2021-12-15 20:52:43 +0000

    Delete IGNORE directory

--- IGNORE/CUCP1.py
+++
@@ -1,7 +0,0 @@
-print("username and password seems vaild")
-r_u = input("Set a new username: ")
-with open("user/username.txt", 'w') as f:
-    f.writelines(r_u)
-r_p = input("Set a new password: ")
-with open("user/password.txt", 'w') as f:
-    f.writelines(r_p)
